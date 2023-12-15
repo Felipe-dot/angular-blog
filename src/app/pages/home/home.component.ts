0cb5c188f2c5a8973c9a05cdc94708196bf07244
Felipe-dot <felipeluis610@gmail.com>
2023-12-15 10:11:09 -0300

    Improve code structure

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { dataFake } from 'src/app/data/dataFake';
+
+
+type DataFake =  {
+      "id": string
+      "title": string
+      "description": string
+      "photoCover":string
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-  constructor() { }
 
+
+export class HomeComponent implements OnInit {
+  data:DataFake[] 
+  firstTwoItems:DataFake[]
+
+  constructor() { 
+    this.data = dataFake;
+    this.firstTwoItems = this.data.slice(0,2)
+  }
+  
   ngOnInit(): void {
+    console.log(this.data)
   }
 
 }
