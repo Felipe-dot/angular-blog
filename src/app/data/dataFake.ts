4602d92d461112dbbf7af18dd226e96c215c96e7
Felipe-dot <felipeluis610@gmail.com>
2023-12-15 10:10:33 -0300

    Increasing mock data

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,33 @@
 export const dataFake = [
     {
-      "id":"1",
-      "title": "NOVO HOMEM DE FERRO EM 3D",
-      "description": "marvel anuncia um novo filme do homem de ferro, confira",
-      "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+      "id":"0",
+      "title": "Kylo Ren não teria redenção em Star Wars, diz Adam Driver",
+      "description": "Astro contou que plano original para o vilão era bem mais sombrio do que foi para a tela, Falando no talk show de Rich Eisen, o astro revelou até que a ideia original era justamente o contrário: fazer Kylo Ren ir mais e mais para o Lado Sombrio com o passar dos filmes      ",
+      "photoCover":"https://uploads.jovemnerd.com.br/wp-content/uploads/2023/12/star_wars_kylo_ren__q2b23i0-1210x544.jpg"
     },
     {
+      "id":"1",
+      "title": "Star Wars: Dave Filoni se torna diretor criativo da LucasFilm",
+      "description": "Pupilo de George Lucas, cineasta é responsável por The Clone Wars, Rebels e o live-action Ahsoka, A LucasFilm, produtora responsável por Star Wars, Indiana Jones e mais, tem um novo diretor criativo: Dave Filoni. Diretor, roteirista e produtor da casa há décadas, ele chegou ao novo cargo após o sucesso de Ahsoka, série protagonizada por muitas de suas criações.",
+      "photoCover":"https://uploads.jovemnerd.com.br/wp-content/uploads/2023/11/star_wars_dave_filoni_diretor_criativo_lucasfilm__3cum057o-1210x544.jpg"
+    },
+
+    {
       "id":"2",
-      "title": "Nova Série anunciada no Disney +",
-      "description": "bla blabla",
-      "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+      "title": "Star Wars de Taika Waititi vai “irritar pessoas”, diz cineasta",
+      "description": "Diretor de Thor: Amor e Trovão está confirmado para um projeto na saga espacial,Mesmo sem novidades há um tempinho, o filme de Star Wars de Taika Waititi segue vivo e, pelo visto, deve causar muito bafafá entre os fãs. Quem diz é o próprio o diretor de Thor: Amor e Trovão, que antecipa reações irritadas ao longa.",
+      "photoCover":"https://uploads.jovemnerd.com.br/wp-content/uploads/2023/11/taika_waititi_star_wars__61s127g-1210x544.jpg"
+    },
+    {
+      "id":"3",
+      "title": "Star Wars de Kevin Feige não vai mais acontecer",
+      "description": "Chefão do MCU não vai mais para uma galáxia muito, muito distante, Chefão do Marvel Studios, Kevin Feige quase colocou os pés numa galáxia muito, muito distante, mas algo deu errado. O produtor do MCU confirmou que não trabalha mais num filme de Star Wars, encerrando o sonho dos nerds que queriam ver uma aventura do Zé Boné na saga espacial.",
+      "photoCover":"https://uploads.jovemnerd.com.br/wp-content/uploads/2023/11/kevin_feige_star_wars_cancelado__1gv82c1fz-1210x544.jpg"
+    },
+    {
+      "id":"4",
+      "title": "Anthony Daniels, o C-3PO de Star Wars, é confirmado na CCXP 2023",
+      "description": "Ator participará do evento nos dias 1, 2 e 3 de dezembro, Nesta quarta-feira (01), a presença de Anthony Daniels foi confirmada na CCXP23. O artista é reconhecido, principalmente, por dar vida ao amado androide C-3PO de Star Wars.",
+      "photoCover":"https://uploads.jovemnerd.com.br/wp-content/uploads/2023/11/c_3po_ccxp23__278g20xz1-1210x544.jpg"
     }
   ]
